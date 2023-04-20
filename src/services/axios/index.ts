a17bb3a8957b5006056be3b3d740c17bb515c3db
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-04-20 01:29:41 +0100

    feat(axios): updates axios client default configuration

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
+// import Config from "react-native-config";
 
-axios.defaults.baseURL = "http://10.0.2.2:8000/api/v1/";
+axios.defaults.baseURL = "https://api.pay-off.overal-x.com/api/v1/";
 
 export default axios.create({});
