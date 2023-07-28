07882ceab03e0cc7f042222be4314c7436da8ff3
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-28 22:05:41 +0100

    build(yarn): installs multiple util dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
-  plugins: ["nativewind/babel"],
+  plugins: [
+    "nativewind/babel",
+    [
+      "module-resolver",
+      {
+        extensions: [".ts", ".tsx"],
+        root: ["."],
+        alias: {
+          "@src": "./src",
+        },
+      },
+    ],
+    ["module:react-native-dotenv"],
+  ],
 };
