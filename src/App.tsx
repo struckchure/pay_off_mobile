996dec4950754eb225922edd0bc520fb36219b93
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-30 08:09:54 +0100

    feat(deep-link): adds deep link handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,38 @@
 import * as Sentry from "@sentry/react-native";
-import React from "react";
-import { Dimensions, StatusBar, View } from "react-native";
+import React, { useEffect } from "react";
+import { Linking, StatusBar, useWindowDimensions } from "react-native";
+import { Div } from "react-native-magnus";
 import { Toast } from "react-native-toast-message/lib/src/Toast";
 
 import { SENTRY_DSN } from "@env";
 import MagnusUIProvider from "@src/components/providers/magus-ui.provider";
 import ReactQueryProvider from "@src/components/providers/react-query.provider";
 import Router from "@src/router";
+import deepLinkHandler from "./router/deep-link-handler";
 
 Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
 });
 
-function App(): JSX.Element {
-  const windowWidth = Dimensions.get("screen").width;
-  const windowHeight = Dimensions.get("screen").height;
+function App() {
+  const { width, height } = useWindowDimensions();
+
+  useEffect(() => {
+    Linking.addEventListener("url", deepLinkHandler);
+
+    return () => {
+      Linking.removeAllListeners("url");
+    };
+  }, []);
 
   return (
     <ReactQueryProvider>
       <MagnusUIProvider>
-        <View className={`w-[${windowWidth}] h-[${windowHeight}]`}>
+        <Div w={width} h={height}>
           <StatusBar />
           <Router />
-        </View>
+        </Div>
         <Toast />
       </MagnusUIProvider>
     </ReactQueryProvider>
---
+++ src/router/deep-link-handler.ts
@@ -0,0 +1,1 @@
+export default function deepLinkHandler(props: any) {}
